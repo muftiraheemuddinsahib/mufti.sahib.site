8dfd795e0a97a07317a6f73868dddb51533e59e9
MUFTI RAHEEMUDDIN SAHIB <mufti.raheemuddin.sahib@gmail.com>
2025-10-31 02:14:00 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   // copy static files (fonts, PDFs)
   eleventyConfig.addPassthroughCopy("static");
+  eleventyConfig.addPassthroughCopy("admin");
   // basic template formats
   eleventyConfig.setTemplateFormats(["md", "njk", "html"]);
   return {
